9b1fa307f3298fc45585a165b178c8764c924a22
Elmar T <elmartirta@rogers.com>
2021-08-22 01:07:40 -0400

    Add Codementor Attribution

--- calculator-app/src/index.js
+++ calculator-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Attribution from './Attribution';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Attribution />
   </React.StrictMode>,
   document.getElementById('root')
 );
