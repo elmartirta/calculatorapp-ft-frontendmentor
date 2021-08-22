7dc41edaeb8bb73cc6354e49352bca9b31e85bf1
Elmar T <elmartirta@rogers.com>
2021-08-22 01:10:16 -0400

    Fix Attribution

--- calculator-app/src/Attribution.js
+++ calculator-app/src/Attribution.js
@@ -1,7 +1,7 @@
 function Attribution() {
     return <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/elmartirta/">Elmar Tirta</a>.
     </div>
 }
 
