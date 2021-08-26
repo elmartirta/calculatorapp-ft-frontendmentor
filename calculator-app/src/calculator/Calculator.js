41517a7225836cffb6d42bac16334482040f6445
Elmar T <elmartirta@rogers.com>
2021-08-25 21:22:19 -0400

    Key clicks naively update screen

--- calculator-app/src/calculator/Calculator.js
+++ calculator-app/src/calculator/Calculator.js
@@ -1,14 +1,22 @@
+import React, {useState} from "react";
 import './Calculator.css'
 import Header from './header/Header'
 import Screen from './screen/Screen'
 import Keypad from'./keypad/Keypad'
 
 function Calculator(props) {
+  const [screenValue, setScreenValue] = useState(399491)
+
+  function onKeyPress(newValue){
+    setScreenValue(newValue);
+  }
+  
+
   return (
     <div id="calculator">
       <Header></Header>
-      <Screen value="399,491"></Screen>
-      <Keypad></Keypad>
+      <Screen value={screenValue}></Screen>
+      <Keypad onKeyPress={onKeyPress}></Keypad>
     </div>
   );
 }
--- calculator-app/src/calculator/keypad/Keypad.js
+++ calculator-app/src/calculator/keypad/Keypad.js
@@ -1,33 +1,38 @@
-function Keypad() {
+function Keypad(props) {
+    function keyPressEvent(e, value){
+        console.log("Firing!" + value);
+        return props.onKeyPress(value)
+    }
+
     return(
         <div id="keypad" class="flex-grid shadowed">
             <div id="keypad-row-1" class= "flex-row noselect">
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button class="emphasized-key">DEL</button>
+                <button onClick={(e) => keyPressEvent(e, 7)}>7</button>
+                <button onClick={(e) => keyPressEvent(e, 8)}>8</button>
+                <button onClick={(e) => keyPressEvent(e, 9)}>9</button>
+                <button onClick={(e) => keyPressEvent(e, "DEL")}class="emphasized-key">DEL</button>
             </div>
             <div id="keypad-row-2" class= "flex-row noselect">
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>+</button>
+                <button onClick={(e) => keyPressEvent(e, 4)}>4</button>
+                <button onClick={(e) => keyPressEvent(e, 5)}>5</button>
+                <button onClick={(e) => keyPressEvent(e, 6)}>6</button>
+                <button onClick={(e) => keyPressEvent(e, "PLUS")}>+</button>
             </div>
             <div id="keypad-row-3" class= "flex-row noselect">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>-</button>
+                <button onClick={(e) => keyPressEvent(e, 1)}>1</button>
+                <button onClick={(e) => keyPressEvent(e, 2)}>2</button>
+                <button onClick={(e) => keyPressEvent(e, 3)}>3</button>
+                <button onClick={(e) => keyPressEvent(e, "MINUS")}>-</button>
             </div>
             <div id="keypad-row-4" class= "flex-row noselect">
-                <button>.</button>
-                <button>0</button>
-                <button>/</button>
-                <button>x</button>
+                <button onClick={(e) => keyPressEvent(e, "DOT")}>.</button>
+                <button onClick={(e) => keyPressEvent(e, 0)}>0</button>
+                <button onClick={(e) => keyPressEvent(e, "DIVIDE")}>/</button>
+                <button onClick={(e) => keyPressEvent(e, "TIMES")}>x</button>
             </div>
             <div id="keypad-row-5" class= "flex-row noselect">
-                <button class="emphasized-key">RESET</button>
-                <button class="strong-key">=</button>
+                <button onClick={(e) => keyPressEvent(e, "RESET")} class="emphasized-key">RESET</button>
+                <button onClick={(e) => keyPressEvent(e, "EQUAL")} class="strong-key">=</button>
             </div>
       </div>
     );
