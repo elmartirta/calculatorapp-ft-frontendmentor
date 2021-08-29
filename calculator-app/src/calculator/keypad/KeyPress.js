15931e6f2a4a74acaafe0d76a402cb8329de0935
Elmar T <elmartirta@rogers.com>
2021-08-29 17:33:09 -0400

    Add PressCategories to KeyPresses

--- calculator-app/src/calculator/keypad/KeyPress.js
+++ calculator-app/src/calculator/keypad/KeyPress.js
@@ -1,28 +1,38 @@
+class PressCategory{
+    static UNDEFINED = new PressCategory("UNDEFINED");
+    static DIGIT = new PressCategory("DIGIT");
+    static OPERAND = new PressCategory("OPERAND");
+    constructor(name){
+        this.name = name;
+    }
+}
+
 class KeyPress {
-    static ONE = new KeyPress("ONE");
-    static TWO = new KeyPress("TWO");
-    static THREE = new KeyPress("THREE");
-    static FOUR = new KeyPress("FOUR");
-    static FIVE = new KeyPress("FIVE");
-    static SIX = new KeyPress("SIX");
-    static SEVEN = new KeyPress("SEVEN");
-    static EIGHT = new KeyPress("EIGHT");
-    static NINE = new KeyPress("NINE");
-    static ZERO = new KeyPress("ZERO");
+    static ONE = new KeyPress("ONE", PressCategory.DIGIT);
+    static TWO = new KeyPress("TWO", PressCategory.DIGIT);
+    static THREE = new KeyPress("THREE", PressCategory.DIGIT);
+    static FOUR = new KeyPress("FOUR", PressCategory.DIGIT);
+    static FIVE = new KeyPress("FIVE", PressCategory.DIGIT);
+    static SIX = new KeyPress("SIX", PressCategory.DIGIT);
+    static SEVEN = new KeyPress("SEVEN", PressCategory.DIGIT);
+    static EIGHT = new KeyPress("EIGHT", PressCategory.DIGIT);
+    static NINE = new KeyPress("NINE", PressCategory.DIGIT);
+    static ZERO = new KeyPress("ZERO", PressCategory.DIGIT);
     
     static DOT = new KeyPress("DOT");
     
-    static PLUS = new KeyPress("PLUS");
-    static MINUS = new KeyPress("MINUS");
-    static DIVIDE = new KeyPress("DIVIDE");
-    static MULTIPLY = new KeyPress("MULTIPLY");
+    static PLUS = new KeyPress("PLUS", PressCategory.OPERAND);
+    static MINUS = new KeyPress("MINUS", PressCategory.OPERAND);
+    static DIVIDE = new KeyPress("DIVIDE", PressCategory.OPERAND);
+    static MULTIPLY = new KeyPress("MULTIPLY", PressCategory.OPERAND);
 
     static DELETE = new KeyPress("DELETE");
     static RESET = new KeyPress("RESET");
     static EQUAL = new KeyPress("EQUAL");
 
-    constructor(name){
+    constructor(name, pressCategory=PressCategory.UNDEFINED){
         this.name = name;
+        this.pressCategory = pressCategory
     }
 }
 
