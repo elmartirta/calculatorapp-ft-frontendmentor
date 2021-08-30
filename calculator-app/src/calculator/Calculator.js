4bd0f28d39d751d949d4b87ca34a013fdcf03189
Elmar T <elmartirta@rogers.com>
2021-08-30 14:51:08 -0400

    Add caching and priming, add screens for both

--- calculator-app/src/calculator/Calculator.js
+++ calculator-app/src/calculator/Calculator.js
@@ -6,13 +6,17 @@ import Keypad from'./keypad/Keypad'
 import PressType from "./keypad/PressType"
 import KeyPress from "./keypad/KeyPress";
 
+
 function Calculator(props) {
   const [activeValue, setActiveValue] = useState([0])
+  const [cachedValue, setCachedValue] = useState([0])
+  const [operand, setOperand] = useState(PressType.UNDEFINED.name);
   const [isPrimed, setIsPrimed] = useState(false)
 
   function pushDigit(arr, keyPress){
     if (isPrimed){
       setIsPrimed(false)
+      setCachedValue(activeValue)
     }
     const newarr = (isPrimed) ? [0] : arr.slice() 
     if (newarr.length === 1 && newarr[0] === 0) {newarr.pop()}
@@ -34,11 +38,32 @@ function Calculator(props) {
     return newArr;
   }
 
+  // function toNum(digitArray){
+  //   return Number(digitArray.join(""))
+  // }
+
+  // function toDigitArray(number){
+  //   return String(number).split("");
+  // }
+
+  // function performArithmetic(keyPress){
+  //   if (keyPress === KeyPress.PLUS){
+  //     setActiveValue((a) => toDigitArray(toNum(cachedValue) + toNum(a)))
+  //   } else if (keyPress === KeyPress.MINUS){
+  //     setActiveValue((a) => toDigitArray(toNum(cachedValue) - toNum(a)))
+  //   } else if (keyPress === KeyPress.MULTIPLY) {
+  //     setActiveValue((a) => toDigitArray(toNum(cachedValue) * toNum(a)))
+  //   } else if (keyPress === KeyPress.DIVIDE) {
+  //     setActiveValue((a) => toDigitArray(toNum(cachedValue) / toNum(a)))
+  //   }
+  // }
+
   function onKeyPress(keyPress){
     if (keyPress.pressCategory === PressType.DIGIT){
       setActiveValue((n) => pushDigit(n, keyPress))
     }else if (keyPress.pressCategory === PressType.OPERAND){
       setIsPrimed(true)
+      setOperand(keyPress.name)
     }else{
       if (keyPress === KeyPress.DELETE){
         setActiveValue((n) => popDigit(n))
@@ -48,6 +73,7 @@ function Calculator(props) {
         setActiveValue((n) => pushDot(n))
       }else if (keyPress === KeyPress.RESET){
         setActiveValue([0])
+        setCachedValue([0])
       }else{
         setActiveValue(["#Missing"])
       }
@@ -58,6 +84,8 @@ function Calculator(props) {
     <div id="calculator">
       <Header></Header>
       <Screen value={activeValue.join("")}></Screen>
+      <Screen value={cachedValue.join("")}></Screen>
+      <Screen value={operand}></Screen>
       <Keypad onKeyPress={onKeyPress}></Keypad>
     </div>
   );
