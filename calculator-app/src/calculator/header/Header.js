5769abab88352d8cfe7fb2fe3d108e35d5117542
Elmar T <elmartirta@rogers.com>
2021-09-01 15:03:15 -0400

    Move theme-label into ThemeToggle class

--- calculator-app/src/calculator/header/Header.js
+++ calculator-app/src/calculator/header/Header.js
@@ -5,10 +5,7 @@ function Header() {
     return (
     <div id="header">
         <h1 id="logo">calc</h1>
-        <div id="theme-selector">
-        <label id="theme-label">THEME</label>
         <ThemeToggle></ThemeToggle>
-        </div>
     </div>
   );
 }
