1874032d3b8cfb4b99ed56c19518284c1b1ba889
Elmar T <elmartirta@rogers.com>
2021-09-24 00:02:08 -0400

    Recolor link color to be more accessible

--- calculator-app/src/Attribution.js
+++ calculator-app/src/Attribution.js
@@ -1,7 +1,9 @@
 function Attribution() {
     return <div className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-        Coded by <a href="https://github.com/elmartirta/">Elmar Tirta</a>.
+        <p>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+            Coded by <a href="https://github.com/elmartirta/">Elmar Tirta</a>.
+        </p>
     </div>
 }
 
